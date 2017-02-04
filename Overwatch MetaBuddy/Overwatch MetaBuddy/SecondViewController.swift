9182da16dc619ce65b64ce6551f50b2b50f739f8
Nathan VelaBorja <n.velaborja@gmail.com>
2017-02-04 12:24:18 -0800

    Added xcode project and image resources

---
+++ Overwatch MetaBuddy/Overwatch MetaBuddy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Overwatch MetaBuddy
+//
+//  Created by Nathan VelaBorja on 2/4/17.
+//  Copyright © 2017 Nathan VelaBorja. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
